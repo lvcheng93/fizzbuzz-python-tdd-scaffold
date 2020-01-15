01a4d71e262ef87489136abb281f87bb867bacf4
lvcheng93 <lvcheng_sjtu@163.com>
2020-01-15 14:38:04 +0800

    fix fizzbuzz: add fizzbuzz function and unit test

--- helloworld.py
+++ helloworld.py
@@ -1,2 +1,13 @@
 def get_greetings():
     return 'Hello World!'
+
+
+def fizzbuzz(i):
+    if i%3==0 and i%5==0:
+        return 'fizzbuzz'
+    elif i%3==0:
+        return 'fizz'
+    elif i%5==0:
+        return 'buzz'
+    else:
+        return str(i)
--- helloworld_test.py
+++ helloworld_test.py
@@ -1,5 +1,10 @@
 from helloworld import get_greetings
+from helloworld import fizzbuzz
 
 
 def test_get_helloworld():
     assert get_greetings() == 'Hello World!'
+    assert fizzbuzz(1) == '1'
+    assert fizzbuzz(3) == 'fizz'
+    assert fizzbuzz(5) == 'buzz'
+    assert fizzbuzz(15) == 'fizzbuzz'
