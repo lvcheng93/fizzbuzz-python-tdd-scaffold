6ff3e092b81217245a6dd88ee2f810872247c3a7
lvcheng93 <lvcheng_sjtu@163.com>
2020-01-15 14:53:34 +0800

    modify fizzbuzz: add fizzbuzz function and unit test

--- helloworld.py
+++ helloworld.py
@@ -3,11 +3,11 @@ def get_greetings():
 
 
 def fizzbuzz(i):
-    if i%3==0 and i%5==0:
+    if i % 3 == 0 and i % 5 == 0:
         return 'fizzbuzz'
-    elif i%3==0:
+    elif i % 3 == 0:
         return 'fizz'
-    elif i%5==0:
+    elif i % 5 == 0:
         return 'buzz'
     else:
         return str(i)
